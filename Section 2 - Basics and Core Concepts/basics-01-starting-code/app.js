e836a36e1e6932ccc488fed0d004b92246444286
Jason Malan <jason.malan@digital2dna.com>
2024-09-15 10:02:46 +0200

    Calling methods in html.

--- Section 2 - Basics and Core Concepts/basics-01-starting-code/app.js
+++ Section 2 - Basics and Core Concepts/basics-01-starting-code/app.js
@@ -1,9 +1,10 @@
 const app = Vue.createApp({
-  data() {
-    return {
-      courseGoal: "Finish the course and learn vue.js!",
-      vueUrl: "https://vuejs.org/",
-    };
+  data: () => ({
+    courseGoal: "Finish the course and learn vue.js!",
+    vueUrl: "https://vuejs.org/",
+  }),
+  methods: {
+    outputGoal: () => (Math.random() > 0.5 ? "Master Vue" : "Learn Vue"),
   },
 });
 
