5008d52c041dd1cb06c6a260ac31292ce0d76a9d
Jason Malan <jason.malan@digital2dna.com>
2024-09-15 10:13:18 +0200

    Random goals.

--- Section 2 - Basics and Core Concepts/basics-01-starting-code/app.js
+++ Section 2 - Basics and Core Concepts/basics-01-starting-code/app.js
@@ -1,10 +1,13 @@
 const app = Vue.createApp({
   data: () => ({
-    courseGoal: "Finish the course and learn vue.js!",
+    courseGoalA: "Finish the course and learn vue.js!",
+    courseGoalB: "Finish the course and master vue.js!",
     vueUrl: "https://vuejs.org/",
   }),
   methods: {
-    outputGoal: () => (Math.random() > 0.5 ? "Master Vue" : "Learn Vue"),
+    outputGoal() {
+      return Math.random() > 0.5 ? this.courseGoalA : this.courseGoalB;
+    },
   },
 });
 
